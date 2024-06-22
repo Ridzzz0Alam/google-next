82d62b6c68bfefa860b61ffbe1d27fae59497751
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-06-22 19:11:33 +0800

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,30 @@
+import Link from "next/link";
 import React from "react";
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  if (!response) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try Searching the web or images for soemthing else{" "}
+        </p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 }
