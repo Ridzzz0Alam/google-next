45fd3c1cf014b559789d89b7d9efbfc697c46fb7
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-06-23 23:50:11 +0800

    added stuffs on settings page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
@@ -11,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="relative min-h-screen">
-        {children}
+        <Suspense>{children}</Suspense>
         <Footer />
       </body>
     </html>
