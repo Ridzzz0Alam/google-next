3a3cfc2bb142e036f00967a8b9ef5cecea77a6c5
Mohammad Reduwan <ridwan0gts@gmail.com>
2024-06-26 02:54:10 +0800

    edited the eslint file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: "export",
   images: {
     remotePatterns: [
@@ -12,4 +15,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
